101b6024384d8a21b83591fa0234852b9c800de7
Théo <t.knopfer@gmail.com>
2025-06-12 13:28:53 +0200

    index form

--- index.php
+++ index.php
@@ -2,11 +2,23 @@
     include_once('environnement.php');
     $title= 'Accueil';
     include_once 'function/head.php';
-    if($_GET['message'] == 'success'){
-        echo '<h1> Inscription réussie!</h1>'
-    }
 ?>
 
 <body>
-    <?php include_once 'layout/header.php'; ?>
+    <?php include_once 'layout/header.php';
+        if(isset($_GET['message']) == 'success'){
+        echo '<h1> Inscription réussie!</h1>';
+    } ?>
+    <main>
+        <div class="login-container">
+            <h2>Rejoindre un tournois</h2>
+            <form action="index.php" method="post">
+                <input placeholder="Chercher un tournois" type="tournois" name="tournois" required>
+                <input placeholder="Votre email" type="email" name="email" required>
+                <input type="text" name="pseudo" placeholder="Votre pseudo">
+                <input id="submit" type="submit" value="Rejoindre ce tournois">
+            </form>
+        </div>
+    </main>
+
 </body>
---
+++ pages/admin.php
@@ -0,0 +1,11 @@
+<main>
+    <div class="login-container">
+        <h2>Rejoindre un tournois</h2>
+        <form action="index.php" method="post">
+            <input placeholder="Chercher un tournois" type="tournois" name="tournois" required>
+            <input placeholder="Votre email" type="email" name="email" required>
+            <input type="text" name="pseudo" placeholder="Votre pseudo">
+            <input id="submit" type="submit" value="Rejoindre ce tournois">
+        </form>
+    </div>
+</main>
